0c279bf40df0946eaeb2c353e7d28da729bd36cf
mackenthiam <didierakossou1@gmail.com>
2026-08-05 16:54:24 +0000

    lorem ipsuim

--- src/posts.js
+++ src/posts.js
@@ -1,12 +1,30 @@
 const blogPosts = [
     {
-        title: "",
-        date: "",
+        title: "Mon article",
+        date: "05/08/2026",
         content: `
-            <p></p>
+            <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco 
+            laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure 
+            dolor in reprehenderit in voluptate velit esse cillum dolore eu 
+            fugiat nulla pariatur.</p>
+            <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa 
+            qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis 
+            unde omnis iste natus error sit voluptatem accusantium doloremque 
+            laudantium.</p>
         `,
-        category: "template",
+        category: "Journal",
         imageLeft: "assets/Images/HD.png",
         imageRight: "assets/Images/jojo5.png"
     },
+    {
+        title: "Lorem Ipsum",
+        date: "05/08/2026",
+        content: `
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        `,
+        category: "Test",
+        imageLeft: "assets/Images/pourtestleft.png",
+        imageRight: "assets/Images/pourtestright.png"
+    }
 ]
